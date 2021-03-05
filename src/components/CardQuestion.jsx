9b887e3aa9b225c2fd17e4bbf9c2196097fd1188
cqrlosArch <diaz.flores.c@gmail.com>
2021-03-05 19:39:09 +0100

    width-card

--- src/components/CardQuestion.jsx
+++ src/components/CardQuestion.jsx
@@ -9,7 +9,7 @@ import Flag from './Flag';
 
 const CardStyled = styled.div`
   background-color: var(--white);
-  max-width: 350px;
+  width: 350px;
   min-height: 415px;
   border-radius: 24px;
   position: relative;
